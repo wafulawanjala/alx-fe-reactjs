1e43123f6f56da581450c59aed88000cad5a9ff7
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-02-23 21:35:55 +0300

    added 3 components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
-import WelcomeMessage from './assets/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from "./components/MainContent"
+import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -10,6 +13,9 @@ function App() {
   return (
     <>
     <WelcomeMessage />
+    <MainContent />
+    <Header />
+    <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
