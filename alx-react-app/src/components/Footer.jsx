575cf717e133892a07a863fa60946afe6df1672f
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-02-23 21:45:30 +0300

    modified components

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,8 @@
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>
+function Footer(){
+    return(
+    <footer>
+        <p>© 2023 City Lovers</p>
+    </footer>
+    )
+};
+export default Footer;
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,3 +1,8 @@
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
+function MainContent(){
+    return(
+    <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+    )
+};
+export default MainContent;
