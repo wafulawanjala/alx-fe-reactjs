919324ae47851050df1887d54e63b497b038644c
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-02-23 21:57:13 +0300

    added a prop

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import MainContent from "./components/MainContent"
 import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -16,6 +17,7 @@ function App() {
     <MainContent />
     <Header />
     <Footer />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
