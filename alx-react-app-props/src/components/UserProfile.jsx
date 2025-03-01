3014e8ffc0d115a9175c8e2115a8ad69eb47fcd7
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-03-02 00:35:19 +0300

    added useConext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,16 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  export default UserProfile
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = () => {
+  const userData = useContext(UserContext); // Get user data from context
+
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
