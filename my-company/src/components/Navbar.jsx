f4e43dd04ba43efba4a0bc32c541b27ee15dd8be
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-03-02 21:34:58 +0300

    added background color

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#333", color: "white", justifyContent:"center", display:"block"}}>
+    <nav style={{ padding: "10px", backgroundColor: "brown", color: "white", justifyContent:"center", display:"block"}}>
       <Link to="/" style={{ margin: "10px", color: "white" }}>Home</Link>
       <Link to="/about" style={{ margin: "10px", color: "white" }}>About</Link>
       <Link to="/services" style={{ margin: "10px", color: "white" }}>Services</Link>
