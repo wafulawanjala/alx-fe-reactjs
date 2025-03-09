2a4d5c8074655b19bb8f9ec8846e4c72551d8849
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-03-09 13:30:42 +0300

    modified

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,11 +2,17 @@ import { create } from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id.toString() !== id) })),
   updateRecipe: (id, updatedRecipe) => set(state => ({
     recipes: state.recipes.map(recipe => recipe.id.toString() === id ? { ...recipe, ...updatedRecipe } : recipe)
   })),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
 }));
-
-export default useRecipeStore;
