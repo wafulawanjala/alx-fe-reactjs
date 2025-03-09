fc804beeb0c7b5603c1d17327ef4d51948f3e375
Wafula Wanjala <wafulawanjala78@gmail.com>
2025-03-09 14:54:46 +0300

    new update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,6 +4,7 @@ const useRecipeStore = create(set => ({
   recipes: [],
   searchTerm: '',
   favorites: [],
+  recommendations: [],
   setSearchTerm: term => set({ searchTerm: term }),
   addRecipe: newRecipe => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: id => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
@@ -12,6 +13,12 @@ const useRecipeStore = create(set => ({
   })),
   addFavorite: recipeId => set(state => ({ favorites: [...state.favorites, recipeId] })),
   removeFavorite: recipeId => set(state => ({ favorites: state.favorites.filter(id => id !== recipeId) })),
+  generateRecommendations: () => set(state => {
+    const recommended = state.recipes.filter(recipe =>
+      state.favorites.includes(recipe.id) && Math.random() > 0.5
+    );
+    return { recommendations: recommended };
+  }),
 }));
 
 export default useRecipeStore;
